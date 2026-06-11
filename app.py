9c6267dd907905f9c6fa104c42d8512bba49b50d
Nehabalkawade <balkawadeneha261@gmail.com>
2026-06-11 15:13:15 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+numbers = [10, 25, 5, 40, 15,23,45,13,97,13,23,11,57,11,56,98,11,13,56,99]
+
+highest = max(numbers)
+lowest = min(numbers)
+average = sum(numbers) / len(numbers)
+
+print("Highest:", highest)
+print("Lowest:", lowest)
+print("Average:", average)
